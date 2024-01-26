ac5a1dbc9d7f32397a32b05ce37eb1dc6cc0bdb9
sopheavatey <2023363heang@aupp.edu.kh>
2024-01-26 15:10:30 +0700

    Update process-string.py

--- process-string.py
+++ process-string.py
@@ -6,6 +6,17 @@
 # * "Hello World" -> "HHeelllloo  WWoorrlldd"
 # * "1234!_ "     -> "11223344!!__  "
 
+user_input = input("Please type in a string of word here: ")
+letter = user_input.split
+
+output_str = ""
+
+for char in user_input:
+    output_str += char * 2
+
+print(output_str)
+
+
 
 # Application 2
 #  Given a string indicating a range of letters, return a string which includes all the letters in that range, including the last letter. Note that if the range is given in capital letters, return the string in capitals also!
@@ -19,6 +30,15 @@
 
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-start, end = alphabet.split('-')
+
 user_range = input("Enter a range of letters (e.g., a-z): ")
+
+start, end = user_range.split('-')
+start_index = alphabet.index(start)
+end_index = alphabet.index(end)
+
+result_str = alphabet[start_index:end_index + 1]
+
+print(result_str)
+
 
